c2b4db95f3e9866608343e06925416a3d18b1efc
Mideny21 <30451596+Mideny21@users.noreply.github.com>
2022-07-04 12:32:02 +0300

    message

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DetailsScreen extends StatefulWidget {
   DetailsScreen({Key? key}) : super(key: key);
@@ -10,36 +11,114 @@ class DetailsScreen extends StatefulWidget {
 class _DetailsScreenState extends State<DetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverList(
-            delegate: SliverChildListDelegate([
-          SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Column(
-                children: [
-                  ClipOval(
-                    child: Image.asset(
-                      'assets/Icons/pana.png',
-                      fit: BoxFit.cover,
-                      width: MediaQuery.of(context).size.width * 0.40,
-                      height: MediaQuery.of(context).size.height * 0.25,
-                    ),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Detail Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+        child: CustomScrollView(
+          slivers: [
+            SliverList(
+                delegate: SliverChildListDelegate([
+              SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      ClipOval(
+                        child: Image.asset(
+                          'assets/images/photo.jpg',
+                          fit: BoxFit.cover,
+                          width: MediaQuery.of(context).size.width * 0.40,
+                          height: MediaQuery.of(context).size.height * 0.25,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      const Text("B&M Store",
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold)),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ClipOval(
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration:
+                                  BoxDecoration(color: Color(0xff407BFF)),
+                              child: Center(
+                                child: Icon(
+                                  Icons.call,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.05),
+                          ClipOval(
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration:
+                                  const BoxDecoration(color: Color(0xff407BFF)),
+                              child: Center(
+                                child:
+                                    Icon(Icons.whatsapp, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.05),
+                          ClipOval(
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration:
+                                  BoxDecoration(color: Color(0xff407BFF)),
+                              child: Center(
+                                child: Icon(Icons.message, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.05),
+                          ClipOval(
+                            child: Container(
+                              width: 50,
+                              height: 50,
+                              decoration:
+                                  const BoxDecoration(color: Color(0xff407BFF)),
+                              child: Center(
+                                child: FaIcon(FontAwesomeIcons.instagram,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Address',
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 20),
-                  const Text("B&M Store",
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                  Row(
-                    children: [],
-                  )
-                ],
-              ),
-            ),
-          )
-        ]))
-      ],
+                ),
+              )
+            ]))
+          ],
+        ),
+      ),
     );
   }
 }
